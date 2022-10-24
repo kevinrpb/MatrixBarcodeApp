c253cb58b7bfb30839dfdcf72e5c8e91926e553c
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2022-10-24 19:06:46 +0200

    Initial Commit

---
+++ MatrixBarcodeApp/MatrixBarcodeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatrixBarcodeAppApp.swift
+//  MatrixBarcodeApp
+//
+//  Created by Romero Peces Barba, Kevin on 24/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MatrixBarcodeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
